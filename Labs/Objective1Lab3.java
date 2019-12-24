c8b1d5d33c62aa2ad1e6f66bcf66e3dd8f2eb175
thehiddenlotus <luke.sutherland@live.com>
2019-12-23 22:26:12 -0800

    lab 3

---
+++ Labs/Objective1Lab3.java
@@ -0,0 +1,7 @@
+public class Objective1Lab3{
+	public static void main(String[] args){
+		System.out.println("Name : Luke Sutherland");
+		System.out.println("Age : 26");
+		System.out.println("State : Hawaii");
+	}
+}
