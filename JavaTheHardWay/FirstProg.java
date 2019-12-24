5af842e3270eec2817618f5a8f0551ab58ca362e
thehiddenlotus <luke.sutherland@live.com>
2019-12-23 19:45:55 -0800

    added file directories

---
+++ JavaTheHardWay/FirstProg.java
@@ -0,0 +1,6 @@
+public class FirstProg {
+	public static void main( String[] args ) {
+		System.out.println( "I Lu, am determined to learn how to code." );
+		System.out.println( "Today's date is 12/22/19" );
+	}
+}
