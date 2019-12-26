83b80f3346a98905e942daf48f5561cf5f63ae4c
thehiddenlotus <luke.sutherland@live.com>
2019-12-25 20:23:52 -0800

    obj 7 labs

---
+++ Labs/Objective7Lab1.java
@@ -0,0 +1,21 @@
+public class Objective7Lab1 {
+  public static void main(String[] args){
+	printName();
+  }
+
+  public static void printName() {
+    System.out.println("Hello Doug");
+  }
+}public class Objective7Lab2 {
+  public static void main(String[] args) {
+    System.out.println("In main");
+    printMessage();
+    System.out.println("In main");
+  }
+
+  public static void printMessage() {
+    /*
+    Your solution goes here
+    */
+  }
+}
